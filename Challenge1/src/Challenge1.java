a702be9c9a27ab2e12b26d0c54c3c69a3cd4e49b
johanruizs <joaruizsa@gmail.com>
2022-11-04 22:25:04 -0500

    Nueva Carpeta

---
+++ Challenge1/src/Challenge1.java
@@ -0,0 +1,6 @@
+public class Challenge1 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+}
