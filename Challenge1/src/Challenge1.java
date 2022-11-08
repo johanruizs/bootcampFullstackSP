031f2c92554a1b75b37e781200977d09cd96d899
johanruizs <joaruizsa@gmail.com>
2022-11-08 17:02:03 -0500

    Primer punto

--- Challenge1/src/Challenge1.java
+++ Challenge1/src/Challenge1.java
@@ -2,22 +2,52 @@
 import java.util.Scanner;
 
 public class Challenge1 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        CodeGenerator();
+    }
+
+
+    public static void CodeGenerator(){
+
+        String codeType;
+        Scanner readType = new Scanner(System.in);
+    
+        System.out.print("Please type in your code type (a or b): ");
+        codeType = readType.nextLine();
+             
+        String code;
+    
+        if (codeType.equalsIgnoreCase("A")){
+            code = "54";
+            for (int i=1; i<=8; i++) {
+                    
+                int number = (int)(Math.random()*10);
+                String a = String.valueOf(number);
+                    
+                code = code + a;	
+            }
+            System.out.println(code);
+    
+            } else if (codeType.equalsIgnoreCase("B")){
+                code = "08";
+                for (int i=1; i<=8; i++) {
+                   
+                   int number = (int)(Math.random()*10);
+                   String a = String.valueOf(number);
+                   
+                   code = code + a;	
+               }
+               System.out.println(code);
+    
+            } else {
+    
+                System.out.println("Your type code is invalid.");
+    
+            }
+    
     }
 }
 
 
-public static void CodeGenerator(){
-
-    String codeType;
-    Scanner readType = new Scanner(System.in);
-
-    System.out.print("Please type in your coe type as follow. ie 'Tipo A' or 'Tipo B': ");
-    codeType = readType.nextLine();
-
-    
 
 
-}
