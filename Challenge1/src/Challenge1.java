eb8283b9084a6d4c140c9dac1886df11c9f682fe
johanruizs <joaruizsa@gmail.com>
2022-11-08 09:55:06 -0500

    Updated code

--- Challenge1/src/Challenge1.java
+++ Challenge1/src/Challenge1.java
@@ -1,6 +1,23 @@
+
+import java.util.Scanner;
+
 public class Challenge1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
     }
+}
+
+
+public static void CodeGenerator(){
+
+    String codeType;
+    Scanner readType = new Scanner(System.in);
+
+    System.out.print("Please type in your coe type as follow. ie 'Tipo A' or 'Tipo B': ");
+    codeType = readType.nextLine();
+
+    
+
+
 }
